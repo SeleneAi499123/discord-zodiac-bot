ff9b4d7dd817febac11fd40ce862391f729414c6
Selene Ai <seleneai499123@gmail.com>
2025-01-21 23:31:04 +0800

    upd

--- index.js
+++ index.js
@@ -131,12 +131,6 @@ client.on("messageCreate", async (message) => {
 
 client.login(config.token);
 
-app.use(express.static('public'))
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
